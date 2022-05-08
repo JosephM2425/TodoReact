1a06eb5cc13978b723fcf5bdc8c4d87ea5b554e2
Joseph Murillo <josephmurillo2425@gmail.com>
2022-05-08 12:52:28 -0600

    changes icons

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { AiOutlineCheckSquare } from "react-icons/ai";
 import { AiFillCheckSquare } from "react-icons/ai";
 import { BsTrashFill } from "react-icons/bs";
 
 function TodoItem({ text, completed, onComplete, onDelete }) {
   return (
     <li className="bg-white relative flex justify-center items-center mt-6 shadow-xl">
-      <span
-        className={`${
-          completed && "text-red-600"
-        } cursor-pointer flex justify-center items-center h-12 w-12  text-3xl font-bold hover:text-red-600`}
-        onClick={onComplete}
-      >
-        <AiFillCheckSquare />
-      </span>
+      {!completed ? (
+        <span
+          className={`${
+            completed && "text-red-600"
+          } cursor-pointer flex justify-center items-center h-12 w-12  text-3xl font-bold hover:text-red-600`}
+          onClick={onComplete}
+        >
+          <AiOutlineCheckSquare />
+        </span>
+      ) : (
+        <span
+          className={`${
+            completed && "text-red-600"
+          } cursor-pointer flex justify-center items-center h-12 w-12  text-3xl font-bold hover:text-red-600`}
+          onClick={onComplete}
+        >
+          <AiFillCheckSquare />
+        </span>
+      )}
+
       <p
         className={`${completed && "text-red-600 line-through"}
         mt-6 mr-0 mb-6 ml-6 w-full text-lg font-normal`}
